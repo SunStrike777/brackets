c518680225c7dee10cbc1f877fe9b565636cc508
SunStrike777 <evgeniy.atroshchenko@gmail.com>
2022-07-16 17:09:43 +0500

    feat: brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let brackets = Object.fromEntries(bracketsConfig);
+
+  for (i = 0; i < str.length; i++) {
+    if (stack.length === 0) {
+      stack.push(str[i]);
+    } else {
+      if (str[i] == brackets[stack[stack.length - 1]]) {
+        stack.pop();
+      } else {
+        stack.push(str[i]);
+      }
+    }
+    console.log(stack);
+  }
+  return stack.length > 0 ? false : true;
+};
